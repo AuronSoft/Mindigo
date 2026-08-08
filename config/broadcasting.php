aab2eebda7c3ba27d7ec0692a8a6d15148a64af6
Mạnh Hưng <buimanhhung3105@gmail.com>
2026-08-08 16:16:58 +0700

    chore: update broadcasting configuration

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_CONNECTION', 'reverb'),
+    'default' => env('BROADCAST_CONNECTION', (env('REVERB_APP_KEY') && env('REVERB_APP_SECRET') && env('REVERB_APP_ID')) ? 'reverb' : 'null'),
 
     /*
     |--------------------------------------------------------------------------
